db123892b6c76cf50dbaa2c1fc2ca54e4b964516
alvindav <alvind@sfu.ca>
2018-03-22 00:41:29 -0700

    removed errors

--- Assignment 2/test.py
+++ Assignment 2/test.py
@@ -4,19 +4,19 @@ import subprocess
 passed = 0
 
 print ("Generating output 1... ")
-subprocess.call("./Tests/dqtest1 > dq1.gt",shell=True)
+subprocess.call("./dqtest1 > ./Tests/dq1.gt",shell=True)
 print ("Generating output 2... ")
-subprocess.call("./Tests/dqtest2 > dq2.gt",shell=True)
+subprocess.call("./dqtest2 > ./Tests/dq2.gt",shell=True)
 print ("Generating output 3... ")
-subprocess.call("./Tests/dqtest3 > dq3.gt",shell=True)
+subprocess.call("./dqtest3 > ./Tests/dq3.gt",shell=True)
 print ("Generating output 4... ")
-subprocess.call("./Tests/dqtest4 > dq4.gt",shell=True)
+subprocess.call("./dqtest4 > ./Tests/dq4.gt",shell=True)
 print ("Generating output 7... ")
-subprocess.call("./Tests/dqtest7 > dq7.gt",shell=True)
+subprocess.call("./dqtest7 > ./Tests/dq7.gt",shell=True)
 
 
 print ("Running test 1... ")
-rt = subprocess.call("diff ./Tests/output1 ./dq1.gt",shell=True);
+rt = subprocess.call("diff ./Tests/output1 ./Tests/dq1.gt",shell=True);
 if rt == 0:
     passed += 1
     print ("passed")
@@ -24,7 +24,7 @@ else:
     print ("failed")
 
 print ("Running test 2... ")
-rt = subprocess.call("diff ./Tests/output2 ./dq2.gt",shell=True);
+rt = subprocess.call("diff ./Tests/output2 ./Tests/dq2.gt",shell=True);
 if rt == 0:
     passed += 1
     print ("passed")
@@ -32,7 +32,7 @@ else:
     print ("failed")
 
 print ("Running test 3... ")
-rt = subprocess.call("diff ./Tests/output3 ./dq3.gt",shell=True);
+rt = subprocess.call("diff ./Tests/output3 ./Tests/dq3.gt",shell=True);
 if rt == 0:
     passed += 1
     print ("passed")
@@ -40,7 +40,7 @@ else:
     print ("failed")
 
 print ("Running test 4... ")
-rt = subprocess.call("diff ./Tests/output4 ./dq4.gt",shell=True);
+rt = subprocess.call("diff ./Tests/output4 ./Tests/dq4.gt",shell=True);
 if rt == 0:
     passed += 1
     print ("passed")
